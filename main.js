efd6018433037367bb7de8864ece4a8e60a97089
Karanamat <100451890+Karanamat@users.noreply.github.com>
2022-12-28 18:07:41 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
-song = "";
-leftWristX = 0;
-leftWristY = 0;
+song1 = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
 rightWristX = 0;
 rightWristY = 0;
 
+leftWristX = 0;
+leftWristY = 0;
+
 function preload()
 {
-  song = loadSound("music.mp3");
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
 }
 
 function setup() {
-    canvas = createCanvas(600, 500);
-    canvas.center();
+	canvas =  createCanvas(600, 500);
+	canvas.center();
 
-    video = createCapture(VIDEO);
-    video.hide();
+	video = createCapture(VIDEO);
+	video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
-    console.log('PoseNet Is Initialized');
+  console.log('PoseNet Is Initialized');
 }
 
 function gotPoses(results)
 {
-    if(results.length > 0)
-    {
+  if(results.length > 0)
+  {
+	console.log(results);
 	scoreRightWrist =  results[0].pose.keypoints[10].score;
 	scoreLeftWrist =  results[0].pose.keypoints[9].score;
-	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 	
 	rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
@@ -39,25 +50,19 @@ function gotPoses(results)
 	leftWristX = results[0].pose.leftWrist.x;
 	leftWristY = results[0].pose.leftWrist.y;
 	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
-    }
-}
-
-function draw() {
-    image(video, 0, 0, 600, 500);
-
-    fill("#FF0000")
-    stroke("#FF0000")
-
-if(scoreLeftWrist > 0.2)
-{
-    circle(leftWristX,leftWristY,20);
-    InNumberleftWristY = Number(leftWristY);
-    volume = floor(InNumberleftWristY)/500;
-    document.getElementById("volume").innerHTML = "Volume = " + volume;
-    song.setVolume(volume);
+		
   }
 }
 
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+}
 function play()
 {
     song.play()
